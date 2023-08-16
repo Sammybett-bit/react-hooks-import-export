a605c0da6974b0c0a66ef19cdf0af6cc2e53ee77
Sammybett-bit <bettsammy20@gmail.com>
2023-08-16 15:20:55 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,28 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {trees, wildlife} from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
+}
+function ColoradoStateParks() {
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
+}
+
+export default ColoradoStateParks;
+console.log(trees);
+wildlife();
+export const trees = "Aspen and Pine";
+export function wildlife() {
+  console.log("Elk, Bighorn Sheep, Moose");
+}
+function elevation() {
+  console.log("9583 ft");
 }
